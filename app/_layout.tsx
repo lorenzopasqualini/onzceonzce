7d75e5793b4d6066bf94be202476a4fd62b79d01
Lorenzo Pasqualini <lorpasqu@gmail.com>
2024-02-04 00:06:21 +0100

    v0.3

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,11 @@ import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import { PaperProvider } from 'react-native-paper';
+import { createContext, useEffect, useState } from 'react';
+import { PaperProvider, TextInput } from 'react-native-paper';
 import { theme } from '@/theme';
+import { SearchableStock } from '@/data';
+import { searchStocks } from '@/utils/searchStocks';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -44,15 +46,41 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+export const StoreContext = createContext<{
+  searchQuery: string;
+  setSearchQuery: (text: string) => void;
+  searchedStocks: SearchableStock[];
+  setSearchedStocks: (stocks: SearchableStock[]) => void
+  }>({
+    searchQuery: "",
+    setSearchQuery: () => {},
+    searchedStocks: [],
+    setSearchedStocks: () => {}
+  })
+
 function RootLayoutNav() {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [searchedStocks, setSearchedStocks] = useState<SearchableStock[]>([])
+
   return (
     <PaperProvider theme={theme}>
       <ThemeProvider value={DarkTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="search" options={{ headerShown: false }} />
-          <Stack.Screen name="[ticker]" options={{ headerShown: false }} />
-        </Stack>
+        <StoreContext.Provider value={{searchQuery, setSearchQuery, searchedStocks, setSearchedStocks}}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="search" options={{ headerTitle: ()=>
+              <TextInput placeholder='Search' mode='outlined' dense autoFocus style={{ width: '88%' }}
+                onChangeText={(text: string)=>{
+                  setSearchQuery(text);
+                  const stocks = searchStocks(text);
+                  setSearchedStocks(stocks)
+                }}
+              />
+              }}
+            />
+            <Stack.Screen name="[ticker]" options={{ headerShown: false }} />
+          </Stack>
+        </StoreContext.Provider>
       </ThemeProvider>
     </PaperProvider>
   );
--- app/search.tsx
+++ app/search.tsx
@@ -1,9 +1,33 @@
-import { View, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { StoreContext } from './_layout';
+import { Text } from 'react-native-paper';
+import { Card } from '@/components/Card';
 
 export default function SearchScreen() {
-  return (
-    <View></View>
-  );
+    const {searchQuery, searchedStocks} = useContext(StoreContext)
+
+    if(!searchQuery && searchedStocks.length === 0){
+        return (
+            <View style={styles.container}>
+                <Text variant='titleLarge' style={{ fontWeight: 'bold' }}>Search Stocks</Text>
+            </View>
+        );
+    }
+
+    if(searchQuery && searchedStocks.length === 0){
+        return (
+            <View style={styles.container}>
+                <Text variant='titleLarge' style={{ fontWeight: 'bold' }}>No stocks found</Text>
+            </View>
+        );
+    }
+
+    return <FlatList
+        data={searchedStocks}
+        keyExtractor={(item)=> item.ticker}
+        renderItem={({item})=> <Card {...item} />}
+    />
 }
 
 const styles = StyleSheet.create({
